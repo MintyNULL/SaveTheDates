e59da1756f749437b618d4e1dfa09366698ee195
changejang <changejang@gmail.com>
2022-06-11 14:15:55 +0900

    :tada: Begin a Project!

--- crawler/main.go
+++ crawler/main.go
@@ -1,38 +1,43 @@
 package main
 
 import (
+	"SaveTheDates/crawler/http"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-type Product struct {
+type Date struct {
 	gorm.Model
-	Code  string
-	Price uint
+	Title   string
+	Year    int8
+	Month   int8
+	Day     int8
+	EnTitle string
 }
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("STD.db"), &gorm.Config{})
 	if err != nil {
 		panic("Db 연결에 실패하였습니다.")
 	}
+	http.Test()
 	// 테이블 자동 생성
-	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&Date{})
 
 	// 생성
-	db.Create(&Product{Code: "D42", Price: 100})
+	db.Create(&Date{Code: "D42", Price: 100})
 
 	// 읽기
-	var product Product
-	db.First(&product, 1)                 // primary key기준으로 product 찾기
-	db.First(&product, "code = ?", "D42") // code가 D42인 product 찾기
+	var date Date
+	db.First(&date, 1)                 // primary key기준으로 product 찾기
+	db.First(&date, "code = ?", "D42") // code가 D42인 product 찾기
 
 	// 수정 - product의 price를 200으로
-	db.Model(&product).Update("Price", 200)
+	db.Model(&date).Update("Price", 200)
 	// 수정 - 여러개의 필드를 수정하기
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"})
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	db.Model(&date).Updates(Date{Price: 200, Code: "F42"})
+	db.Model(&date).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
 
 	// 삭제 - product 삭제하기
-	db.Delete(&product, 1)
+	db.Delete(&date, 1)
 }
